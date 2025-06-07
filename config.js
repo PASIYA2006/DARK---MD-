60c586903809f2f031ab5547500a5dee0dc80fb0
PASIYA2006 <pasirashmika294@gmail.com>
2025-06-07 22:42:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,4 +8,5 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID,
 ALIVE_IMG: process.env.ALIVE_IMG || "", 
 ALIVE_MSG: process.env.ALIVE_IMG || "HELLO, I AM DARK-MD",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "True",
 };
