9260fea6cb31e200a1f93bacd53a2812d0d32513
PASIYA2006 <pasirashmika294@gmail.com>
2025-06-07 22:21:52 +0530

    Update song+video.js

--- plugins/song+video.js
+++ plugins/song+video.js
@@ -1,1 +1,41 @@
+const {cmd , commands} = require('../command')
+const fg = require(`api-dylux`)
+const yts = require(`yt-search`)
+cmd({
+    pattern: "song",
+    desc: "download songs",
+    category: "download",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if(!q) return reply("please give me url or title")
+const search = await yts(q)
+const data = search.video[0];
+const url = data.url
 
+let desc = `
+DARK-MD SONG DOWNLOADING...
+
+title: ${data.title}
+description: ${data.description}
+time: ${data.timestamp}
+ago: ${data.ago}
+views: ${data.views}
+
+MADE BY PaZiYa ⭕️
+`
+await conn.sendMessage(from,{image:{url: data.thumbnail},caption:desc},{quoted:mek})
+
+//download audio
+
+let down = await fg.yta(url)
+let downloadUrl = down.dl_url
+
+//send audio message
+await conn.sendMessage(from,{ausio: {url:downloadUrl},mimetype:"audio/mpeg"},{quoted:mek})
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
