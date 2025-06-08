51e4b59a5003e9bb07947480d1bb22ce37ab622b
PASIYA2006 <pasirashmika294@gmail.com>
2025-06-08 19:09:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const axios = require('axios')
 const { File } = require('megajs')
 const prefix = '.'
 
-const ownerNumber = ['94717210988']
+const ownerNumber = ['94701322956']
 
 //===================SESSION-AUTH============================
 if (!fs.existsSync(__dirname + '/auth_info_baileys/creds.json')) {
