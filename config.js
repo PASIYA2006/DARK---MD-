0c728e3c6c2b35e57a330964c401cf7148d13c57
PASIYA2006 <pasirashmika294@gmail.com>
2025-06-08 18:01:35 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
-ALIVE_IMG: process.env.ALIVE_IMG || "", 
+ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/PasiYa73/Photo/blob/main/%40r07qxo%20-%20R%E2%A4%93Download.jpeg", 
 ALIVE_MSG: process.env.ALIVE_IMG || "HELLO, I AM DARK-MD",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "True",
 };
