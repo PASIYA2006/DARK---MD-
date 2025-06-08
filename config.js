6ee9de0ca91714959ec592b52349220f4e2de3b9
PASIYA2006 <pasirashmika294@gmail.com>
2025-06-08 18:49:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,4 +9,5 @@ SESSION_ID: process.env.SESSION_ID,
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/PasiYa73/Photo/blob/main/%40r07qxo%20-%20R%E2%A4%93Download.jpeg", 
 ALIVE_MSG: process.env.ALIVE_IMG || "HELLO, I AM DARK-MD",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "True",
+MODE: process.env.MODE || "public",
 };
